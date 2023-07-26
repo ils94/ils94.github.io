f2dbbe97f4351d066f3901ff4abe28f35b78b9c2
ILS <99141005+ils94@users.noreply.github.com>
2023-07-26 16:08:25 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const jsonData = {
-    "imagem_perfil": "https://m.media-amazon.com/images/I/71+mDoHG4mL.png",
-    "nome": "Nome",
-    "profissao": "Profissão",
+    "imagem_perfil": "https://avatars.githubusercontent.com/u/99141005?v=4",
+    "nome": "Igor Lima e Silva",
+    "profissao": "Programador Hobbista",
     "redes_sociais": {
-      "github": "https://github.com/seu-usuario-do-github",
-      "linkedin": "https://www.linkedin.com/seu-usuario-do-linkedin"
+      "github": "https://github.com/ils94",
+      "linkedin": "https://www.linkedin.com/"
     }
   };
