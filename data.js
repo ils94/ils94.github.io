c2d36a436719e5e85fc3a051f1b6b8b2b6f2220d
ILS <99141005+ils94@users.noreply.github.com>
2023-10-06 13:00:14 -0300

    Delete data.js

--- data.js
+++
@@ -1,9 +0,0 @@
-const jsonData = {
-    "imagem_perfil": "https://hips.hearstapps.com/hmg-prod/images/how-to-keep-ducks-call-ducks-1615457181.jpg?crop=0.669xw:1.00xh;0.166xw,0&resize=1200:*",
-    "nome": "ILS",
-    "profissao": "Programador Hobbista",
-    "redes_sociais": {
-      "github": "https://github.com/ils94",
-      "linkedin": "https://www.linkedin.com/"
-    }
-  };
